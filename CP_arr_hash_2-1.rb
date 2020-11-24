d8c80b9221abe15bd48d0ed0bc9182bfb5175ada
chiaki_yamaguchi <ayanottyz@gmail.com>
2020-11-24 16:50:01 +0900

    ファイル追加

---
+++ CP_arr_hash_2-1.rb
@@ -0,0 +1,16 @@
+# 配列の要素の出現頻度をもとめる
+# 文字列を要素とした配列を受け取り、要素名をキーに出現回数を値としたハッシュを返す
+
+# 実行例
+# count(["a", "b", "c", "a", "c", "c"]) -> {"a"=>2, "b"=>1, "c"=>3}
+# count(["a", "b", "c", "a", "b", "c"]) -> {"a"=>2, "b"=>2, "c"=>2}
+
+def count(array)
+  hash = Hash.new()
+  narr = array.uniq  # 配列の重複を削除した配列narrを作成  
+  for s in narr do
+    hash[s] = array.count(s)  # narrの各値についてarray内での出現回数をcount～hashの値に代入
+  end
+  
+  return hash
+end
